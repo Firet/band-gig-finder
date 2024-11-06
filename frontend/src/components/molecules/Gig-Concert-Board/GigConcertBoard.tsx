4dfa8ebf7d31272c78915fc15d12a2c06725c743
Firet <damianfederico@protonmail.com>
2024-11-06 19:56:51 -0300

    Render event date in html

--- frontend/src/components/molecules/Gig-Concert-Board/GigConcertBoard.tsx
+++ frontend/src/components/molecules/Gig-Concert-Board/GigConcertBoard.tsx
@@ -1,11 +1,12 @@
 import GigEvents from "@/constants/Event";
-export default function GigConcertBoard({dateTitle= 'Hoy'}) {
+export default function GigConcertBoard({ dateTitle = 'Hoy' }) {
     const gigEvents = GigEvents;
     return (
         <>
             <section className="bg-black p-2 mx-6">
                 <h1 className="bg-amber-400">{dateTitle}</h1>
-                <h2> {gigEvents[0].title}</h2>
+                <h2> {gigEvents[0].title} el {gigEvents[0].event_date.getDate()}
+                    &ensp;de&ensp;{gigEvents[0].event_date.toLocaleDateString('default', { month: 'long' })}</h2>
                 <h2>Poseidótica en Morón</h2>
                 <h2>Babasónicos en Lanús</h2>
             </section>
