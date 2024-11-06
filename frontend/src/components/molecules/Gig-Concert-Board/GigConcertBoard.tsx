f76caf1b730780a0bbb3233d0435851c3a3e3cdd
Firet <damianfederico@protonmail.com>
2024-11-06 19:31:22 -0300

    Create Event constant in front end

--- frontend/src/components/molecules/Gig-Concert-Board/GigConcertBoard.tsx
+++ frontend/src/components/molecules/Gig-Concert-Board/GigConcertBoard.tsx
@@ -1,9 +1,11 @@
-
+import Event from "@/constants/Event";
 export default function GigConcertBoard({dateTitle= 'Hoy'}) {
+    const Eventa = Event;
     return (
         <>
             <section className="bg-black p-2 mx-6">
                 <h1 className="bg-amber-400">{dateTitle}</h1>
+                <h2> {Eventa[0].title}</h2>
                 <h2>Poseidótica en Morón</h2>
                 <h2>Babasónicos en Lanús</h2>
             </section>
---
+++ frontend/src/constants/Event.ts
@@ -0,0 +1,20 @@
+const Event = [
+    {
+        title: 'Gigafest',
+        body: '',
+        user_id: 1,
+        status: '',
+        created_at: {
+            gigDayNumber: 1,
+            gigDayName: 'Martes',
+            gigMonth: 'Brumario',
+        },
+        event_date: {
+            gigDayNumber: 18,
+            gigDayName: 'Martes',
+            gigMonth: 'Brumario',
+        },
+    }
+];
+
+export default Event;
