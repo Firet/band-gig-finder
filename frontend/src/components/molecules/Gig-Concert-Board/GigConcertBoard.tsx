bbc0762eed2b4d766b9920d035b228f9468145e9
Firet <damianfederico@protonmail.com>
2024-11-14 08:55:15 -0300

    Fix 'november' and 'noviembre' not matching server-rendered text

--- frontend/src/components/molecules/Gig-Concert-Board/GigConcertBoard.tsx
+++ frontend/src/components/molecules/Gig-Concert-Board/GigConcertBoard.tsx
@@ -16,7 +16,7 @@ export default function GigConcertBoard({ dateTitle = 'Hoy' }) {
             <section className="bg-black p-2 mx-6">
                 <h1 className="bg-amber-400">{dateTitle}</h1>
                 <h2> {gigEvents[0].title} el {gigEvents[0].event_date.getDate()}
-                    &ensp;de&ensp;{gigEvents[0].event_date.toLocaleDateString('default', { month: 'long' })}</h2>
+                    &ensp;de&ensp;{gigEvents[0].event_date.toLocaleDateString('es-ES', { month: 'long' })}</h2>
                 <h2>Poseidótica en Morón</h2>
                 <h2>Babasónicos en Lanús</h2>
             </section>
